8cb71bd0f405fd83a2c5ea490c184d34c3c5e95b
Murilo Niéri <murilo.nieri@hotmail.com>
2015-08-28 08:36:48 -0300

    Refactory da classe SondaEntity

--- src/main/java/br/com/munieri/desafio/explorando/marte/infrastructure/repository/sonda/SondaEntity.java
+++ src/main/java/br/com/munieri/desafio/explorando/marte/infrastructure/repository/sonda/SondaEntity.java
@@ -1,10 +1,12 @@
 package br.com.munieri.desafio.explorando.marte.infrastructure.repository.sonda;
 
+import br.com.munieri.desafio.explorando.marte.domain.sonda.Coordenada;
 import br.com.munieri.desafio.explorando.marte.domain.sonda.Sonda;
+
 import javax.persistence.*;
 
 @Entity
-public class SondaEntity implements Sonda{
+public class SondaEntity implements Sonda {
 
     @Id
     @GeneratedValue
@@ -19,6 +21,10 @@ public class SondaEntity implements Sonda{
     @Column
     private String posicaoAtual;
 
+    private Integer eixoX;
+    private Integer eixoY;
+    private Integer direcao;
+
     public SondaEntity() {
     }
 
@@ -26,6 +32,7 @@ public class SondaEntity implements Sonda{
         this.id = id;
         this.posicao = posicao;
         this.comandos = comandos;
+        splitCaracteres(posicao);
     }
 
     @Override
@@ -48,9 +55,41 @@ public class SondaEntity implements Sonda{
         return posicaoAtual;
     }
 
-    @Override
-    public void posicaoAtual(String posicaoAtual) {
-        this.posicaoAtual = posicaoAtual;
+    public void viraDireita() {
+        direcao = (direcao - 1) < Coordenada.NORTH.val() ? Coordenada.WEST.val() : direcao - 1;
+    }
+
+    public void viraEsquerda() {
+        direcao = (direcao + 1) > Coordenada.WEST.val() ? Coordenada.NORTH.val() : direcao + 1;
+    }
+
+    public void mover() {
+        if (direcao == Coordenada.NORTH.val()) {
+            this.eixoY++;
+        } else if (direcao == Coordenada.EAST.val()) {
+            this.eixoX++;
+        } else if (direcao == Coordenada.SOUTH.val()) {
+            this.eixoY--;
+        } else if (direcao == Coordenada.WEST.val()) {
+            this.eixoX--;
+        }
+    }
+
+    private void splitCaracteres(String posicao) {
+
+        String palavras[] = posicao.split(" ");
+
+        this.eixoX = Integer.parseInt(palavras[0]);
+        this.eixoY = Integer.parseInt(palavras[1]);
+        this.direcao = getDirecao(palavras[2]);
+    }
+
+    private Integer getDirecao(String direcao) {
+        return Coordenada.direcaoIntValue(direcao);
+    }
+
+    public void atualizaDirecaoAtual() {
+        this.posicaoAtual = eixoX + " " + eixoY + " " + Coordenada.direcaoStringValue(direcao);
     }
 }
 
